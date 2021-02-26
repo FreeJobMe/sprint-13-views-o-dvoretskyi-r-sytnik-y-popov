d6f3f1d1275490dea08ab01a14ac0aeffd6c81e0
yuurrr <wisestdev@gmail.com>
2021-02-26 14:41:40 +0200

    Product Entity and Repo

---
+++ Distribution.DAL/Infrastructure/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Distribution.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribution.DAL.Infrastructure.Interfaces
+{
+	public interface IProductRepository
+	{
+		public Product GetById(int id);
+	}
+}
---
+++ Distribution.DAL/Infrastructure/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Distribution.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Distribution.DAL.Infrastructure
+{
+	public class ProductRepository
+	{
+		private List<Product> products = new List<Product>();
+
+		public ProductRepository()
+		{
+			Product.nextId = 0;
+			products.AddRange(new List<Product>() {
+				new Product("Bread", 10), 
+				new Product("Milk", 11)});
+		}
+
+		public Product GetById(int id) => 
+			products.First(p => p.Id == id);
+	}
+}
