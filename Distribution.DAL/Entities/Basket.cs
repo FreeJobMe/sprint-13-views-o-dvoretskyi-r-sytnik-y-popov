785d3bbd233ca7a5bd334772371557eec0f5bc53
Roman Sytnik <romasytnikw@gmail.com>
2021-02-26 16:19:14 +0200

    Added User and Basket entities

---
+++ Distribution.DAL/Entities/Basket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribution.DAL.Entities
+{
+    public class Basket
+    {
+        public static int nextId;
+
+        public int Id { get; set; }
+        
+        public DateTime DateTime{ get; set; }
+
+        private List<Position> positions  = new List<Position>();
+        public Basket(int userId,List<Position> postions)
+        {
+            Id = nextId++;
+            postions = this.positions;
+            DateTime = DateTime.Now;
+        }
+    }
+}
---
+++ Distribution.DAL/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribution.DAL.Entities
+{
+    public class User
+    {
+        public static int nextId;
+
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        private List<Basket> baskets = new List<Basket>();
+
+        public User(string fullName)
+        {
+            Id = nextId++;
+            FullName = fullName;
+        }
+    }
+}
