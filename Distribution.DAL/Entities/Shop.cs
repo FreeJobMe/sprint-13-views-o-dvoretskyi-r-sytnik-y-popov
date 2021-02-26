331dad50c073bd603c7b540dec4cb2dba5844d41
Oleksandr Dvoretskyi <sashadv7@gmail.com>
2021-02-26 16:32:28 +0200

    Added Shop entity and repository. Product repo supplemented with new products

---
+++ Distribution.DAL/Entities/Shop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Distribution.DAL.Entities
+{
+    public class Shop
+    {
+        public static int nextId;
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Position> Positions { get; set; }
+
+        public Shop(string title, List<Position> positions)
+        {
+            Id = nextId++; 
+            Title = title;
+            Positions = positions;
+        }
+    }
+}
