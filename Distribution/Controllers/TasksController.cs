1750444e9f8bc45950ffbc07e417ad46f8b13b7b
Roman Sytnik <romasytnikw@gmail.com>
2021-02-27 20:02:09 +0200

    Added 1,2,3 Tasks

---
+++ Distribution/Controllers/TasksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Distribution.UI.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult SprintTasks()
+        {
+            return View();
+        }
+        public IActionResult Greetings()
+        {
+            ViewBag.Value = "C# Marathon!";
+            ViewBag.Greeting = "Welcome to our project!";
+            return View();
+        }
+    }
+}
